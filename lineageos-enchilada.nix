d1e30ad0a3c9891fa7b866283dbc11192b50c492
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-08-31 19:42:04 -0400

    Add enchilada build

---
+++ lineageos-enchilada.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ./base
+  ];
+
+  # These are required options, but commented out here since I set it programmatically for my devices elsewhere
+  device = "enchilada";
+  flavor = "lineageos";
+  androidVersion = 11; # Pin to lineageos-18.1 for now
+}
